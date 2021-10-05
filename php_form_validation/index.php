554438bb828565e50d8a622bb2d3ec7f23dddf91
Simao Moreno <saiman13moreno@gmail.com>
2021-10-05 22:25:01 +0100

    some changes

--- php_form_validation/index.php
+++ php_form_validation/index.php
@@ -6,6 +6,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Form Validation</title>
+
+    <?php
+    // define variables and set to empty values
+    $name = $email = $gender = $comment = $website = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = test_input($_POST["name"]);
+        $email = test_input($_POST["email"]);
+        $website = test_input($_POST["website"]);
+        $comment = test_input($_POST["comment"]);
+        $gender = test_input($_POST["gender"]);
+    }
+
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    ?>
 </head>
 
 <body>
@@ -19,7 +40,15 @@
         <input type="radio" name="gender" value="female">Female</input></br>
         <input type="radio" name="gender" value="male">Male</input></br>
         <input type="radio" name="gender" value="other">Other</input></br>
+
+        <input type="submit">
     </form>
+
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            echo $_POST["name"];
+        }
+    ?>
 </body>
 
 </html>
